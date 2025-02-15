d48822bd174cae918ba604a0b156aee1b0bef6af
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2025-02-15 15:39:04 -0300

    clear correto

--- api/server.js
+++ api/server.js
@@ -132,6 +132,11 @@ app.post("/write", async (req, res) => {
     const auth = await authorize();
     const sheets = google.sheets({ version: "v4", auth });
 
+    await sheets.spreadsheets.values.clear({
+      spreadsheetId: SPREADSHEET_ID,
+      range: "A:D", // Limpa todas as colunas usadas
+    });
+
     await sheets.spreadsheets.values.update({
       spreadsheetId: SPREADSHEET_ID,
       range: "A1",
