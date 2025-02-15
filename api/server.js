25f57131bf57e5984e8bb96339fac618e76edfae
Joao Emanoel Rodrigues Mota <jooemanoel@gmail.com>
2025-02-14 23:28:17 -0300

    ajustando o json

--- api/server.js
+++ api/server.js
@@ -1,25 +1,25 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require("json-server");
+import { create, router as _router, defaults, rewriter } from "json-server";
 
-const server = jsonServer.create();
+const server = create();
 
 // Uncomment to allow write operations
-const fs = require("fs");
-const path = require("path");
-const filePath = path.join("db.json");
-const data = fs.readFileSync(filePath, "utf-8");
+import { readFileSync } from "fs";
+import { join } from "path";
+const filePath = join("db.json");
+const data = readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
-const router = jsonServer.router(db);
+const router = _router(db);
 
 // Comment out to allow write operations
 // const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults();
+const middlewares = defaults();
 
 server.use(middlewares);
 // Add this before server.use(router)
 server.use(
-  jsonServer.rewriter({
+  rewriter({
     "/api/*": "/$1",
     "/blog/:resource/:id/show": "/:resource/:id",
   })
@@ -30,4 +30,4 @@ server.listen(3000, () => {
 });
 
 // Export the Server API
-module.exports = server;
+export default server;
